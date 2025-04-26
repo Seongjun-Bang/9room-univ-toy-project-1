a9e636e2dfa34b8fc368143d8ff80cb135cdc55c
minju kim <min020227@gmail.com>
2025-04-26 15:40:46 +0900

    main + location

--- toy-project/src/App.js
+++ toy-project/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import SignUpOCR from './components/SignUpOCR.js';
 import Login from './components/Login.js';
 import Register from './components/Register.js';
+import Main from './components/main.js';
+import LabLocation from './components/LabLocation.js';
 
 function App() {
   return (
     <div className="App">
-          <Register/>
+          <LabLocation />
     </div>
   );
 }
--- toy-project/src/components/main.js
+++ toy-project/src/components/main.js
@@ -1,1 +1,53 @@
-//sdfsdf
+import React from 'react';
+import './css/Main.css';
+import CarIcon from '../assets/Car.svg';
+import PinIcon from '../assets/Pin.svg';
+import PersonIcon from '../assets/Person.svg';
+
+function Main() {
+  return (
+    <div className="home-container">
+      <header className="home-header">학과사전</header>
+
+      <section className="section">
+        <h3>인기글</h3>
+        <div className="post-list">
+          <div className="post-item">
+            <p className="post-title">하 안되겠다</p>
+            <p className="post-sub">교수님 ....</p>
+            <div className="post-meta">
+              <span>💬 37</span>
+              <spa className='post-heart'>❤️ 158</spa>
+              <span className='post-major'>AI데이터전공</span>
+            </div>
+          </div>
+
+          <div className="post-item">
+            <p className="post-title">문자 뭐여 ㅋㅋㅋ</p>
+            <p className="post-sub">[돔: 동막생활상담센터]</p>
+            <div className="post-meta">
+              <span>💬 10</span>
+              <span className='post-heart'>❤️ 130</span>
+              <span className='post-major'>컴퓨터공학과</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <h3>과방 위치</h3>
+  <div className="small-card lab-card">
+    <img src={PinIcon} alt="pin" className="lab-icon" />
+    <div className="lab-info">
+      <strong>컴퓨터공학과</strong>
+      <p>공대 다동&nbsp;&nbsp;4층 A411호</p>
+    </div>
+  </div>
+
+
+      </section>
+    </div>
+  );
+}
+
+export default Main;
