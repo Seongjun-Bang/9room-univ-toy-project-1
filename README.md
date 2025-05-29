73e5f150f7d23732d99b6eff421cf094f7526e3d
minJUu-kiM <min020227@gmail.com>
2025-05-29 11:38:39 +0900

    toy-project

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# 구름톤 유니브 학과사전
+# 9room 대학생 커뮤니티 (9room Univ Community)
+
+대학생들을 위한 커뮤니티 플랫폼입니다.  
+자유롭게 글을 쓰고, 정보를 공유하고, 소통할 수 있는 공간을 제공합니다.
+
+---
+
+## ✨ 주요 기능
+
+- 📮 커뮤니티 게시판 (자유, 정보, 홍보 등)
+- 🔍 글 정렬 및 필터링 기능 (최신순 / 인기순 / 평점순)
+- 📝 게시글 작성 및 삭제
+- 📍 과방 찾기
+- 🧭 과방 제보
+- 📁 OCR 인증
+
+---
+
+## 🛠 기술 스택
+
+- **Frontend**: React
+- **State Management**: React Hooks
+- **Routing**: React Router
+- **HTTP Client**: Axios
+
+---
+
+## 🧑‍💻 실행 방법
+
+1. 레포지토리 클론
+
+   ```bash
+   git clone https://github.com/Seongjun-Bang/9room-univ-toy-project-1.git
+
+   ```
+
+2. 디렉토리 이동 및 패키지 설치
+
+   ```
+   cd toy-project
+   npm install
+
+   ```
+
+3. 개발 서버 실행
+   ```
+   npm start
+   ```
+
+---
+
+## 👥 팀원
+
+- 최민호 - PM
+- 김민주 - Frontend, Design
+- 방성준 - Frontend
+- 문준원 - Backend
--- toy-project/src/components/Header.js
+++ toy-project/src/components/Header.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import './css/Header.css';
+import React from "react";
+import "./css/Header.css";
 //import './css/header.css';
 import { SlArrowLeft } from "react-icons/sl";
 
-
 // Header.js
-const Header = ({ title = '', onClose, buttonLabel, onButtonClick }) => {
+const Header = ({ title = "", onClose, buttonLabel, onButtonClick }) => {
   return (
     <>
       <div className="header-wrapper">
         <div className="header-container">
-          
           {/* 왼쪽 버튼 */}
           <button className="header-close" onClick={onClose}>
-          <SlArrowLeft />
-        </button>
+            <SlArrowLeft />
+          </button>
 
           {/* 가운데 타이틀 */}
           <h2 className="header-title">{title}</h2>
@@ -28,8 +26,8 @@ const Header = ({ title = '', onClose, buttonLabel, onButtonClick }) => {
             <div className="header-right-space" />
           )}
         </div>
-       {/* 빨간 구분선: header-container 바로 아래에 넣기 */}
-       <hr className="write-divider" />
+        {/* 빨간 구분선: header-container 바로 아래 넣기 */}
+        <hr className="write-divider" />
       </div>
     </>
   );
